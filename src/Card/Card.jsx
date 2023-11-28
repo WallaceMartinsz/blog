f9e3fb3417efdea81c5471948d976dbad76942e9
Wallace Martins <martinsswallace@gmail.com>
2023-11-28 13:23:57 -0300

    add: icons to delete and update

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import './Card.css'
+import { FaTrash, FaEdit } from 'react-icons/fa';
+import './Card.css';
 
-function Card({ tipo, titulo, descricao, autor, imagemSrc }) {
-    return (
-        <div className="card">
-            <div className="card__left">
-                <span>{tipo}</span>
-                <h1>{titulo}</h1>
-                <p>{descricao}</p>
-                <span>{autor}</span>
-            </div>
-            <div className="card__right">
-                <img src={imagemSrc} alt="Imagem do card" width={300} height={300}/>
-            </div>
+function Card({ tipo, titulo, descricao, autor, imgSrc, onDelete, onUpdate }) {
+  return (
+    <div className="card">
+      <div className="card__left">
+      <span>{autor}</span>
+        <span>{tipo}</span>
+        <h1>{titulo}</h1>
+        <p>{descricao}</p>
+      </div>
+      <div className="card__right">
+        <img src={imgSrc} alt="Imagem do card" width={300} height={300} />
+        <div className="icons">
+          <FaEdit className="edit-icon" onClick={onUpdate} />
+          <FaTrash className="delete-icon" onClick={onDelete} />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Card;
