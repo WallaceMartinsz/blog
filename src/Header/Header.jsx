f6b362a1a8e50285dddaf1d0e88cb22c7ed864ff
Wallace Martins <martinsswallace@gmail.com>
2023-11-28 13:23:37 -0300

    feat: add link to routes

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
-function Header(){
-    return(
-        <header className='cabecalho'>
-            <h1 className='titulo1'>NASSAU</h1>
-            <h1 className='titulo2'>BLOG</h1>
-        </header>
-    )
+function Header() {
+  return (
+    <header className='cabecalho'>
+      <div className='titulo'>
+          <h1 className='titulo1'>NASSAU</h1>
+          <h1 className='titulo2'>BLOG</h1>
+      </div>
+      <nav className='links'>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/posts">Posts</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
